2601e020cd3aa1d9445b17b833a0d7f5114b2751
GitPhillip <216011438@student.uj.ac.za>
2021-01-23 23:06:33 +0200

    Proxy stuff

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const api = axios.create({
-    baseURL: '/api',
+    baseURL: `${PROCESS.ENV.PROXY}/api`,
     headers: {
         'Content-Type': 'application/json'
     },
