b5275f2417fd8a37f57129ba382e82d7220054fa
GitPhillip <216011438@student.uj.ac.za>
2021-01-27 21:24:07 +0200

    Navbar toggling fixes

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SideBar() {
 
     return (
 
-           // {/*<!-- Sidebar -->*/}
-            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            //{/*<!-- Sidebar -->*/}
+            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion  " id="accordionSidebar">
 
                 {/*<!-- Sidebar - Brand --> "images/favicon.ico"*/}
-                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin/dashboard">
+                <Link class="sidebar-brand d-flex align-items-center justify-content-center" to="/admin/dashboard">
                 <img src="/images/logo.jpg" alt='Resurgent' width='44px' />
                     <div class="sidebar-brand-text mx-3">Resurgent Logistics</div>
-                </a>
+                </Link>
 
                 {/*<!-- Divider -->*/}
                 <hr class="sidebar-divider my-0" />
 
                 {/*<!-- Nav Item - Dashboard -->*/}
                 <li class="nav-item active">
-                    <a class="nav-link" href="/admin/dashboard">
+                    <Link class="nav-link" to="/admin/dashboard">
                         <i class="fas fa-fw fa-tachometer-alt"></i>
-                        <span>Dashboard</span></a>
+                        <span>Dashboard</span></Link>
                 </li>
 
                 {/*<!-- Divider -->*/}
@@ -33,24 +34,24 @@ export default function SideBar() {
 
                 {/*<!-- Personal Profile Management -->*/}
                 <li class="nav-item">
-                    <a class="nav-link" href="/admin/profile" >
+                    <Link class="nav-link" to="/admin/profile" >
                         <i class="fas fa-fw fa-user"></i>
                         <span>Personal Profile</span>
-                    </a>
+                    </Link>
                 </li>
 
                 {/*<!-- Personal Profile Management -->*/}
                 <li class="nav-item">
-                    <a class="nav-link collapsed" href data-toggle="collapse" data-target="#collapseUserManagement"
+                    <Link class="nav-link collapsed" to data-toggle="collapse" data-target="#collapseUserManagement"
                         aria-expanded="true" aria-controls="collapseUserManagement">
                         <i class="fas fa-fw fa-users"></i>
                         <span>User Management</span>
-                    </a>
+                    </Link>
                     <div id="collapseUserManagement" class="collapse" aria-labelledby="headingUserManagement" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
                             <h6 class="collapse-header">Manage users:</h6>
-                            <a class="collapse-item" href="/admin/registrations">Registrations</a>
-                            <a class="collapse-item" href="/admin/user_management">User Management</a>
+                            <Link class="collapse-item" to="/admin/registrations">Registrations</Link>
+                            <Link class="collapse-item" to="/admin/user_management">User Management</Link>
                         </div>
                     </div>
                 </li>
@@ -66,16 +67,16 @@ export default function SideBar() {
 
                 {/*<!-- Nav Item - Assets -->*/}
                 <li class="nav-item">
-                    <a class="nav-link collapsed" href data-toggle="collapse" data-target="#collapseAssets"
+                    <Link class="nav-link collapsed" data-toggle="collapse" data-target="#collapseAssets"
                         aria-expanded="true" aria-controls="collapseAssets">
                         <i class="fas fa-fw fa-truck"></i>
                         <span>Asset Management</span>
-                    </a>
+                    </Link>
                     <div id="collapseAssets" class="collapse" aria-labelledby="headingTwoAssets" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
                             <h6 class="collapse-header">Devices:</h6>
-                            <a class="collapse-item" href="/admin/asset_types">Asset Types</a>
-                            <a class="collapse-item" href="/admin/asset_management">Asset Management</a>
+                            <Link class="collapse-item" to="/admin/asset_types">Asset Types</Link>
+                            <Link class="collapse-item" to="/admin/asset_management">Asset Management</Link>
                         </div>
                     </div>
 
@@ -83,16 +84,16 @@ export default function SideBar() {
 
                 {/*<!-- Nav Item - Pages Collapse Menu -->*/}
                 <li class="nav-item">
-                    <a class="nav-link collapsed" href data-toggle="collapse" data-target="#collapseDevices"
+                    <Link class="nav-link collapsed"  data-toggle="collapse" data-target="#collapseDevices"
                         aria-expanded="true" aria-controls="collapseDevices">
                         <i class="fas fa-fw fa-cog"></i>
                         <span>Device Management</span>
-                    </a>
+                    </Link>
                     <div id="collapseDevices" class="collapse" aria-labelledby="headingTwoDevices" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
                             <h6 class="collapse-header">Devices:</h6>
-                            <a class="collapse-item" href="/admin/device_types">Device Types</a>
-                            <a class="collapse-item" href="/admin/device_management">Device Management</a>
+                            <Link class="collapse-item" to="/admin/device_types">Device Types</Link>
+                            <Link class="collapse-item" to="/admin/device_management">Device Management</Link>
                         </div>
                     </div>
                 </li>
@@ -108,9 +109,9 @@ export default function SideBar() {
 
                 {/*<!-- Nav Item - Charts -->*/}
                 <li class="nav-item">
-                    <a class="nav-link" href="/admin/customers">
+                    <Link class="nav-link" to="/admin/customers">
                     <i class="fas fa-handshake"></i>
-                        <span>Customers</span></a>
+                        <span>Customers</span></Link>
                 </li>
 
                 {/*<!-- Divider -->*/}
@@ -124,16 +125,16 @@ export default function SideBar() {
 
                 {/*<!-- Nav Item - Charts -->*/}
                 <li class="nav-item">
-                    <a class="nav-link" href="/admin/log_history">
+                    <Link class="nav-link" to="/admin/log_history">
                         <i class="fas fa-fw fa-clock"></i>
-                        <span>Log History</span></a>
+                        <span>Log History</span></Link>
                 </li>
 
                 {/*<!-- Divider -->*/}
                 <hr class="sidebar-divider d-none d-md-block" />
 
             </ul>
-          // {/*<!-- End of Sidebar -->*/}
+        //   {/*<!-- End of Sidebar -->*/}
 
     )
 }
